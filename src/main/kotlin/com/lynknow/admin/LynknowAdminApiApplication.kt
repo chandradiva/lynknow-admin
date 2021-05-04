55d78806a9699be8a82949667350aab7b682baa5
Fredilla Prima Chandradiva <Fredilla_PC175@mitrais.com>
2021-05-04 23:03:52 +0700

    initial commit

---
+++ src/main/kotlin/com/lynknow/admin/LynknowAdminApiApplication.kt
@@ -0,0 +1,11 @@
+package com.lynknow.admin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LynknowAdminApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LynknowAdminApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/lynknow/admin/LynknowAdminApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lynknow.admin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LynknowAdminApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
